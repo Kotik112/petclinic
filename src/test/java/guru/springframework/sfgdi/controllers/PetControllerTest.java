5900aca6485bc1f0ffc2f5ebda263025650bea83
Arman I <88910492+Kotik112@users.noreply.github.com>
2023-01-03 03:20:22 +0100

    PetGreetingService

---
+++ src/test/java/guru/springframework/sfgdi/controllers/PetControllerTest.java
@@ -0,0 +1,20 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.PetService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PetControllerTest {
+
+    PetController controller;
+    PetService petService;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void whichPetIsTheBest() {
+    }
+}
