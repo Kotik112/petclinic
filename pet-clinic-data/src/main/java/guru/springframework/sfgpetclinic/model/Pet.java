b35870749674ab4bac4b347e62c240828e868c56
Arman I <88910492+Kotik112@users.noreply.github.com>
2023-01-03 04:36:35 +0100

    deleted scraps

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -1,53 +1,32 @@
 package guru.springframework.sfgpetclinic.model;
 
-import lombok.*;
-import org.springframework.format.annotation.DateTimeFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
 
-/**
- * Created by jt on 7/13/18.
- */
+
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
-@Table(name = "pets")
-public class Pet extends BaseEntity{
+public class Pet {
 
-    @Builder
-    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
-        super(id);
-        this.name = name;
-        this.petType = petType;
-        this.owner = owner;
-        this.birthDate = birthDate;
-
-        if (visits == null || visits.size() > 0 ) {
-            this.visits = visits;
-        }
-    }
-
-    @Column(name = "name")
+    //@Column(name = "name")
     private String name;
 
-    @ManyToOne
-    @JoinColumn(name = "type_id")
+    //@ManyToOne
+    //@JoinColumn(name = "type_id")
     private PetType petType;
 
-    @ManyToOne
-    @JoinColumn(name = "owner_id")
+    //@ManyToOne
+    //@JoinColumn(name = "owner_id")
     private Owner owner;
 
-    @Column(name = "birth_date")
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
-    private Set<Visit> visits = new HashSet<>();
+    //@OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
 
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,30 +1,10 @@
 package guru.springframework.sfgpetclinic.model;
 
-import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+public class PetType {
 
-/**
- * Created by jt on 7/13/18.
- */
 
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name = "types")
-public class PetType extends BaseEntity {
-
-    @Builder
-    public PetType(Long id, String name) {
-        super(id);
-        this.name = name;
-    }
-
-    @Column(name = "name")
+    //@Column(name = "name")
     private String name;
 
 
